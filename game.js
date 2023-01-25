4bade99aec6061adb3209f552d978a5894ae5bcb
loordbarringtn <quantas@gmail.com>
2023-01-26 00:01:03 +0300

    final

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ const marble = (() => {
       return null;
     }
     if (checkIfNumber(playerGuess)) {
-      alert('Неверный ввод!');
+      alert("Неверный ввод!");
       return getPlayerGuess();
     }
     playerGuess = parseInt(playerGuess);
@@ -29,8 +29,8 @@ const marble = (() => {
 
   const updateBalls = (playerGuess, botGuess) => {
     if (
-      (playerGuess % 2 === 0 && botGuess === 'нечетное') ||
-      (playerGuess % 2 !== 0 && botGuess === 'четное')
+      (playerGuess % 2 === 0 && botGuess === "нечетное") ||
+      (playerGuess % 2 !== 0 && botGuess === "четное")
     ) {
       result.botBalls -= playerGuess;
       result.playerBalls += playerGuess;
@@ -46,13 +46,14 @@ const marble = (() => {
   };
 
   const getBotGuess = () => {
-    return Math.random() < 0.5 ? 'четное' : 'нечетное';
+    return Math.random() < 0.5 ? "четное" : "нечетное";
   };
 
   const game = () => {
-    if (result.playerBalls === 0) {
+    debugger;
+    if (result.playerBalls <= 0) {
       return false;
-    } else if (result.botBalls === 0) {
+    } else if (result.botBalls <= 0) {
       return true;
     }
     let playerGuess = getPlayerGuess();
@@ -71,16 +72,13 @@ const marble = (() => {
                  \tКоличество шаров у игрока: ${result.playerBalls}\t
                  \tКоличество шаров у компьютера: ${result.botBalls}\t`);
     const resultGame = game();
-    if (resultGame === null) alert('Выходим из игры!');
-    return false;
-    if (resultGame === true) {
+    if (resultGame === null) alert("Выходим из игры!");
+    else if (resultGame === true) {
       alert(`Поздравляем, вы выиграли! Отличная работа!`);
     } else if (resultGame === false) {
       alert(`Вы проиграли! Повезет в следующий раз!`);
-      return false;
     }
   };
-
   return startGame();
 })();
 
